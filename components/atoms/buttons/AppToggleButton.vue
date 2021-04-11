598312abdefc706c49a222e3d8f1831f3f19ffc9
bonybody <yumaaa312@gmail.com>
2021-04-11 18:41:40 +0900

    firebase SDKのバージョンを更新

---
+++ components/atoms/buttons/AppToggleButton.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "AppToggleButton"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/molecules/commons/TheGlobalMenu.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "GlobalMenu"
+}
+</script>
+
+<style scoped>
+
+</style>
