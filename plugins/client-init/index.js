e4bfb5e3cb8d3237d7aa1be510738e7d60140ab2
bonybody <yumaaa312@gmail.com>
2021-04-21 06:25:45 +0900

    client initのLIFFログイン後のロジックを変更

--- plugins/client-init/index.js
+++ plugins/client-init/index.js
@@ -1,21 +1,21 @@
 import liff from "@/plugins/liff";
 
-export default async function ({redirect, store, $fire, $axios,}, inject) {
-  await liff.init({liffId: process.env.liffId})
-  // liffでログイン中かつfirebaseでログインしていない場合
-  inject('liff', liff)
-  if (await liff.isLoggedIn() && !store.getters.isLoggedIn) {
-    // liffのuIdを取得
-    const accessToken = await liff.getAccessToken()
-    const baseUrl = process.env.apiUrl + '/api/custom_token'
-    const query = `?accessToken=${accessToken}`
-    const getTokenUrl = encodeURI(baseUrl + query)
-    try {
-      const customToken = await $axios.$get(getTokenUrl)
-      await $fire.auth.signInWithCustomToken(customToken)
-    } catch (e) {
-      console.log(e.message)
+export default async function ({redirect, store, $fire, $axios}, inject) {
+  await liff.init({liffId: process.env.liffId}).then(async () => {
+    inject('liff', liff)
+    if (liff.isLoggedIn() && !store.getters.isLoggedIn) {
+      // liffのuIdを取得
+      const accessToken = await liff.getAccessToken()
+      const baseUrl = process.env.apiUrl + '/api/custom_token'
+      const query = `?accessToken=${accessToken}`
+      const getTokenUrl = encodeURI(baseUrl + query)
+      try {
+        const customToken = await $axios.$get(getTokenUrl)
+        await $fire.auth.signInWithCustomToken(customToken)
+      } catch (e) {
+        console.log(e.message)
+      }
     }
-  }
-  document.getElementById('first-view-animation').classList.add('onload')
+    document.getElementById('first-view-animation').classList.add('onload')
+  })
 }
