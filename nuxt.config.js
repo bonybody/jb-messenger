29b68fe2fc36a53037d875e7998cd44cee9f61c0
bonybody <yumaaa312@gmail.com>
2021-04-21 09:58:09 +0900

    htmlにlang=ja属性を追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ const isDev = process.env.NODE_ENV === 'development'
 export default {
   head: {
     title: 'my-scheduling',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
